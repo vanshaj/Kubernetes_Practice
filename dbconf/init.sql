006b490dc7c92c18dac276a3f4e6a7ac3c1e6278
Vanshaj <vanshajdhar@gmail.com>
2023-05-28 12:17:51 +0530

    completed first phase\ 1. Create a stateful set with PVC and PV 2. Now if we start a stateful set and delete it still the data will remain with the PV 3. Created a node port service for the code running on local to communicate with the database

---
+++ dbconf/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    dept VARCHAR(255) NOT NULL
+);
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    task_name VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL REFERENCES users(id)
+);
--- webserver/app/run.go
+++ webserver/app/run.go
@@ -3,6 +3,8 @@ package app
 import (
 	"database/sql"
 	"fmt"
+	"os"
+	"strconv"
 
 	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
@@ -11,8 +13,18 @@ import (
 
 func Run() error {
 	log.Info().Msg("Starting webserver")
+	// get env host, port, user, password, dbname
+	host := os.Getenv("DB_HOST")
+	port, bool := os.LookupEnv("DB_PORT")
+	if !bool {
+		port = "5432"
+	}
+	portNumber, _ := strconv.Atoi(port)
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		"localhost", 5432, "", "", "")
+		host, portNumber, user, password, dbname)
 	dbCon, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Error().Msg(err.Error())
